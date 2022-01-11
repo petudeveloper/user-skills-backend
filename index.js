8e272f96d5a0d0a0091cbcd8ce35b36f21859690
petumazo <davidalvarezmazzo@gmail.com>
2022-01-11 15:22:43 -0500

    Use bcrypt to hash passwords.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var express = require('express');
 var cors = require('cors');
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const app = express();
 
 app.use(express.json());
@@ -15,23 +18,35 @@ app.post('/register', (req, res) => {
   const username = req.body.username
   const password = req.body.password
 
-  const user1 = new User({ username: username, password: password });
-  user1.save().then(() => console.log('user created successfully'));
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err)
+    }
+    const user1 = new User({ username: username, password: hash });
+    user1.save().then(() => res.send("User created succesfully"));
+  })
+
 });
 
 app.post('/login', (req, res) => {
   const username = req.body.username
   const password = req.body.password
 
-  User.find({ username: username, password: password }, (err, data) => {
+  User.find({ username: username }, (err, data) => {
     if (err) {
       res.send(err)
     }
     
     if (data.length > 0) {
-      res.send(data)
+      bcrypt.compare(password, data[0].password, (err, response) => {
+        if (response) {
+          res.send(data)
+        } else {
+          res.send({ message: "Wrong username/password combination" })
+        }
+      })
     } else {
-      res.send({ message: "Wrong username/password combination" })
+      res.send({ message: "User doesn't exist" })
     }
   });
 });
